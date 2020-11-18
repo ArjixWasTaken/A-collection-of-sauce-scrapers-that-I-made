5b4f4eaa47c3579fd8938de8be2ad5b4e0764eed
Arjix <53124886+ArjixGamer@users.noreply.github.com>
2020-11-18 17:42:53 +0200

    Update sankaku.py

--- sankaku.py
+++ sankaku.py
@@ -5,12 +5,13 @@ import os
 import json
 
 
-folder = '.'
-
-
 class Scraper:
-    def __init__(self):
+    def __init__(self, folder):
         self.link = 'https://capi-v2.sankakucomplex.com/posts/keyset'
+        self.download_dir = folder
+        if not os.path.isdir(self.download_dir):
+            os.makedirs(self.download_dir)
+
         self.params = {
             "lang": "en",
             "default_threshold": 1,
@@ -18,53 +19,36 @@ class Scraper:
             "tags": "rating:18"
         }
         self.headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; rv:68.0) Gecko/20100101 Firefox/68.0'
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; rv:68.0) Gecko/20100101 Firefox/68.0'  # noqa
             }
         self.page_num = 1
 
-    def nextPage(self):
-        self.page_num += 1
-
-    def getJSON(self):
-        r = requests.get(self.link, headers=self.headers, params=self.params).json()
-        self.params['next'] = r['meta']['next']
-
-        return r['data']
-
     def getImages(self):
-        images = [x['file_url'] for x in self.getJSON()]
+        JSON = requests.get(self.link, headers=self.headers, params=self.params).json()  # noqa
+        images = [x['file_url'] for x in JSON['data']]
+        self.params['next'] = JSON['meta']['next']
+        self.page_num += 1
         return images
 
-
-class Media:
-    def __init__(self, link):
-        self.src = link
-        self.filepath = os.path.join(folder, self.src.split('?')[0].split('/')[-1])
-        self.headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; rv:68.0) Gecko/20100101 Firefox/68.0'
-            }
-
-    def download(self):
-        if not os.path.isfile(self.filepath):
-            if os.path.isfile(self.filepath + '.temp'):
-                os.remove(self.filepath + '.temp')
-            with open(self.filepath + '.temp', 'wb') as f:
-                print('Downloading: {}.'.format(self.filepath))
-                f.write(requests.get(self.src, headers=self.headers).content)
-            os.replace(self.filepath + '.temp', self.filepath)
+    def downloadMedia(self, link):
+        filepath = os.path.join(self.download_dir, link.split('?')[0].split('/')[-1]).replace('\\', '/')  # noqa
+        if not os.path.isfile(filepath):
+            if os.path.isfile(filepath + '.temp'):
+                os.remove(filepath + '.temp')
+            with open(filepath + '.temp', 'wb') as f:  # noqa
+                print('Downloading: {}'.format(filepath.split('/')[-1]))
+                f.write(requests.get(link, headers=self.headers).content)
+            os.replace(filepath + '.temp', filepath)
         else:
-            print(f'Skipping download of: {self.filepath}.')
-
-
-def dl(link):
-    Media(link).download()
+            print("Skipping download: {}".format(filepath.split('/')[-1]))
 
 
 @click.command()
 @click.option('--pages', '-p', required=False, default=0)
-def main(pages):
+@click.option('--download-dir', '-d', 'folder', required=False, default='./sankaku', type=str)  # noqa
+def main(pages, folder):
     imgs = []
-    scraper = Scraper()
+    scraper = Scraper(folder)
 
     if not pages:
         pages = [1]
@@ -74,12 +58,10 @@ def main(pages):
         print('Current page: {}'.format(scraper.page_num))
         posters = scraper.getImages()
 
-        results = ThreadPool(15).imap_unordered(dl, posters)
+        results = ThreadPool(15).imap_unordered(scraper.downloadMedia, posters)
         for i in results:
             pass
 
-        if len(pages) > 1:
-            scraper.nextPage()
     print('Done!')
 
 
